60a6baf06dffb01704e74ad9e751763be1f0c9bf
Dmitriy <tsukikohaya@gmail.com>
2022-04-02 14:59:08 +0300

    create postForm component (emit, props, etc)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,25 @@
 <template>
   <div class="app">
-    <form @submit.prevent>
-      <input
-        type="text"
-        placeholder="Название поста"
-        v-model="title">
-      <input
-        type="text"
-        placeholder="Описание поста"
-        v-model="body">
-      <button
-        class="btn"
-        @click="createPost">Добавить
-      </button>
-    </form>
+    <post-form @create="createPost"/>
     <post-list :posts="posts"/>
   </div>
 </template>
 
 <script>
 import postList from "@/components/postList";
-
+import postForm from "@/components/postForm";
 export default {
   components: {
-    postList
+    postList, postForm
   },
   data() {
     return {
       posts: [],
-      title: '',
-      body: ''
     }
   },
   methods: {
-    createPost() {
-      let newPost = {
-        id: Date.now(),
-        title: this.title,
-        body: this.body
-      };
-      this.posts.push(newPost);
-      this.title = '';
-      this.body = '';
+    createPost(post) {
+      this.posts.push(post)
     }
   }
 }
@@ -58,24 +36,5 @@ export default {
   padding: 15px;
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-}
-
-form input {
-  margin-bottom: 10px;
-  border: 2px solid teal;
-  padding: 10px 15px;
-}
-
-.btn {
-  margin-bottom: 25px;
-  padding: 10px 15px;
-  background: none;
-  color: teal;
-  border: 2px solid teal;
-  align-self: flex-end;
-}
 
 </style>
---
+++ src/components/postForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form @submit.prevent>
+    <input
+      type="text"
+      placeholder="Название поста"
+      v-model="post.title">
+    <input
+      type="text"
+      placeholder="Описание поста"
+      v-model="post.body">
+    <button
+      class="btn"
+      @click="createPost">Добавить
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      post: {
+        body: '',
+        title: ''
+      }
+    }
+  },
+  methods: {
+    createPost() {
+      this.post.id = Date.now()
+      this.$emit('create', this.post)
+      this.post = {
+        title: '',
+        body: ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+form input {
+  margin-bottom: 10px;
+  border: 2px solid teal;
+  padding: 10px 15px;
+}
+
+.btn {
+  margin-bottom: 25px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 2px solid teal;
+  align-self: flex-end;
+  cursor: pointer;
+}
+
+</style>
