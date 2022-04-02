25df625ca591e3f5e98a7f6884c2652d37bb99ce
Dmitriy <tsukikohaya@gmail.com>
2022-04-02 14:24:26 +0300

    create postList component

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,17 @@
         @click="createPost">Добавить
       </button>
     </form>
-    <div class="post"
-         v-for="post in posts"
-         v-if="posts.length">
-      <div><strong>Название: </strong>{{ post.title }}</div>
-      <div><strong>Описание: </strong>{{ post.body }}</div>
-    </div>
-    <h3 v-else>Постов нет</h3>
+    <post-list :posts="posts"/>
   </div>
 </template>
 
 <script>
+import postList from "@/components/postList";
+
 export default {
+  components: {
+    postList
+  },
   data() {
     return {
       posts: [],
@@ -79,9 +78,4 @@ form input {
   align-self: flex-end;
 }
 
-.post {
-  margin-top: 5px;
-  padding: 10px 15px;
-  border: 2px solid teal;
-}
 </style>
---
+++ src/components/postList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="post"
+       v-for="post in posts"
+       v-if="posts.length">
+    <div><strong>Название: </strong>{{ post.title }}</div>
+    <div><strong>Описание: </strong>{{ post.body }}</div>
+  </div>
+  <h3 v-else>Постов нет</h3>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.post {
+  margin-top: 5px;
+  padding: 10px 15px;
+  border: 2px solid teal;
+}
+</style>
