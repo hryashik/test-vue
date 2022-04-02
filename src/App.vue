a872d8de30c725bcbb6492c5be64960176de3e3b
Dmitriy <tsukikohaya@gmail.com>
2022-04-02 13:58:54 +0300

    main component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
-  <h1>{{intro}}</h1>
+  <div class="app">
+    <form @submit.prevent>
+      <input
+        type="text"
+        placeholder="Название поста"
+        v-model="title">
+      <input
+        type="text"
+        placeholder="Описание поста"
+        v-model="body">
+      <button
+        class="btn"
+        @click="createPost">Добавить
+      </button>
+    </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        intro: 'hello vue!'
-      }
-    },
+export default {
+  data() {
+    return {
+      posts: [],
+      title: '',
+      body: ''
+    }
+  },
+  methods: {
+    createPost() {
+      let newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      };
+      this.posts.push(newPost);
+      this.title = '';
+      this.body = '';
+    }
   }
+}
 </script>
 
 <style>
+* {
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
 
+.app {
+  padding: 15px;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+form input {
+  margin-bottom: 10px;
+  border: 2px solid teal;
+  padding: 10px 15px;
+}
+
+.btn {
+  margin-bottom: 25px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 2px solid teal;
+  align-self: flex-end;
+}
+
+.post {
+  margin-top: 5px;
+  padding: 10px 15px;
+  border: 2px solid teal;
+}
 </style>
