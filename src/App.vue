a7050d019e14d059cdd0e9fc1476f505bed1f337
Dmitriy <tsukikohaya@gmail.com>
2022-04-02 13:30:03 +0300

    test hello world

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{intro}}</h1>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        intro: 'hello vue!'
+      }
+    },
+  }
+</script>
+
+<style>
+
+</style>
